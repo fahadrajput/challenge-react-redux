123a23419cb56b3e803f5aa28f84fd292e82ee44
Rajput <mansoorhussainrajput888@gmail.com>
2020-02-20 17:14:46 +0500

    /machines/:machineId done

--- src/Machines.js
+++ src/Machines.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import 'antd/dist/antd.css';
-import { Table, Progress, Button } from 'antd';
+import { Table, Progress, Input, Button } from 'antd';
 import { useHistory, useParams } from 'react-router-dom'
 import axios from 'axios';
 
@@ -27,48 +27,76 @@ const columns = [
 export default function Machines() {
 	const [loading, setLoading] = useState(true)
 	const [data, setData] = useState([])
-	const [currentMachine, setCurrentMachine] = useState([])
+	const [currentMachine, setCurrentMachine] = useState({})
+	const [isUpdate, setIsUpdate] = useState(true)
 	const history = useHistory()
 	const params = useParams()
 
 	useEffect(() => {
-		axios.get('http://localhost:8080/machines')
-			.then((result) => {
-				let { data } = result
-				data = data.map((v) => {
-					v.key = v.id
-					return v
+		if (params.machineId) {
+			axios.get(`http://localhost:8080/machines/${params.machineId}`)
+				.then((res) => {
+					setCurrentMachine(res.data)
+					console.log('res')
+					// handleUpdate(res.data)
 				})
-				setData(data)
-				setLoading(false)
-			})
-	})
-
-	if (params.machineId) {
-		return (
-			<div>
-				<h1></h1>
-			</div>
-		)
-	}
+		}
+		else {
+			axios.get('http://localhost:8080/machines')
+				.then((result) => {
+					let { data } = result
+					data = data.map((v) => {
+						v.key = v.id
+						return v
+					})
+					setData(data)
+					setLoading(false)
+				})
+		}
+	}, [isUpdate])
 
 	return (
 		<div>
-			<div>Machines view</div>
-			<Table
-				columns={columns}
-				dataSource={data}
-				size="middle"
-				pagination={false}
-				loading={loading}
-				onRow={(record, rowIndex) => {
-					return {
-						onClick: () => {
-							history.push(`/machines/${record.id}`)
-						}
-					};
-				}}
-			/>
+			{params.machineId ? <div style={{ display: 'flex' }}>
+				<div style={{ flex: 1 }}>
+					<h1>{currentMachine.name}</h1>
+					<h2>Update Device</h2>
+					<h3>Name:</h3>
+					<Input value={currentMachine.name} style={{ width: '80%', marginBottom: 20, display: 'block' }} />
+					<div style={{ display: 'flex', justifyContent: 'flex-end', marginRight: '20%' }}>
+						<Button type="primary">Submit</Button>
+					</div>
+				</div>
+				<div style={{ flex: 1, marginTop: 20 }}>
+					
+					<div style={{ width: '80%', borderWidth: 1, borderColor: 'black', borderStyle: 'solid', marginBottom: 20 }}>
+						<h2 style={{ textAlign: 'center', marginTop: 10 }}>{currentMachine.health}</h2>
+						<div style={{ display: 'flex', justifyContent: 'center' }}>
+							<div className="progress" style={{ width: '90%', marginBottom: 20 }}>
+								<div className="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style={{ width: '70%' }}>
+									<span className="sr-only">70% Complete</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<h1>Stats</h1>
+					<h4>IP Address: {currentMachine.ip_address}</h4>
+				</div>
+			</div> : <Table
+					columns={columns}
+					dataSource={data}
+					size="middle"
+					pagination={false}
+					loading={loading}
+					onRow={(record, rowIndex) => {
+						return {
+							onClick: () => {
+								setCurrentMachine(record)
+								history.push(`/machines/${record.id}`)
+							}
+						};
+					}}
+				/>}
 		</div>
 	);
 }
